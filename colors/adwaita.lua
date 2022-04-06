c8e5f0749084b236819d31c19ea6ceb1a0b470a3
mofiqul <mofi0islam@gmail.com>
2022-04-07 00:17:25 +0530

    Added light variant

--- colors/adwaita.lua
+++ colors/adwaita.lua
@@ -1,4 +1,5 @@
-local theme = require'adwaita.dark'
+local adwaita_dark = require'adwaita.dark'
+local adwaita_light = require'adwaita.light'
 
 vim.cmd("hi clear")
 
@@ -6,8 +7,13 @@ if vim.fn.exists("syntax_on") then
 	vim.cmd("syntax reset")
 end
 
-vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.g.colors_name = "adwaita"
 
-theme.set()
+if vim.g.adwaita_mode == 'dark' then
+    vim.o.background = "dark"
+    adwaita_dark.set()
+else
+    vim.o.background = "light"
+    adwaita_light.set()
+end
---
+++ lua/lualine/themes/adwaita.lua
@@ -0,0 +1,42 @@
+-- Copyright (c) 2020-2021 Mofiqul Islam
+-- MIT license, see LICENSE for more details.
+local adwaita = {}
+local colors = {}
+
+if vim.g.adwaita_mode == 'dark' then
+    colors.bg = '#303030'
+    colors.fg = '#DEDDDA'
+else
+    colors.bg = '#F6F5F4'
+    colors.fg = '#3D3846'
+end
+
+adwaita.normal = {
+    b = { fg = colors.fg, bg = colors.bg },
+    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+    c = { fg = colors.fg, bg = colors.bg },
+}
+
+adwaita.visual = {
+    a = { fg = colors.bg, bg = colors.fg, gui = 'bold' },
+    b = { fg = colors.fg, bg = colors.bg },
+}
+
+adwaita.inactive = {
+    b = { fg = colors.bg, bg = colors.fg },
+    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+}
+
+adwaita.replace = {
+    b = { fg = colors.fg, bg = colors.bg },
+    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+    c = { fg = colors.fg, bg = colors.bg },
+}
+
+adwaita.insert = {
+    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+    b = { fg = colors.fg, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
+}
+
+return adwaita
