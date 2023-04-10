d95f7ed0591885fa276d63a79e66049b2575f9bd
mofiqul <mofi0islam@gmail.com>
2023-04-10 08:39:27 +0530

    fix: fixed lualine theme

--- lua/lualine/themes/adwaita.lua
+++ lua/lualine/themes/adwaita.lua
@@ -1,6 +1,6 @@
 -- Copyright (c) 2020-2021 Mofiqul Islam
 -- MIT license, see LICENSE for more details.
-local u = require'adwaita.utils'
+local u = require 'adwaita.utils'
 local adwaita = {}
 local colors = {}
 
@@ -13,7 +13,7 @@ local fg = vim.o.background == 'dark' and colors.light_2 or colors.dark_3
 
 adwaita.normal = {
     a = { fg = bg, bg = colors.teal_5, gui = 'bold' },
-    b = { fg = colors.teal_2, bg = bg },
+    b = { fg = colors.teal_2, bg = colors.dark_4 },
     c = { fg = fg, bg = bg },
 }
 
@@ -29,13 +29,13 @@ adwaita.inactive = {
 
 adwaita.replace = {
     a = { fg = bg, bg = colors.purple_4, gui = 'bold' },
-    b = { fg = colors.purple_2, bg = bg },
+    b = { fg = colors.purple_2, bg = colors.dark_4 },
     c = { fg = fg, bg = bg },
 }
 
 adwaita.insert = {
     a = { fg = bg, bg = colors.orange_4, gui = 'bold' },
-    b = { fg = colors.orange_1, bg = vim.o.background == 'dark' and bg or fg },
+    b = { fg = colors.orange_1, bg = vim.o.background == 'dark' and colors.dark_4 or fg },
     c = { fg = fg, bg = bg },
 }
 
